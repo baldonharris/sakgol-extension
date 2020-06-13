e4033ff409d73db7d485a24f7b352898c1fd8086
Harris C. Baldon <hbaldon93@gmail.com>
2020-06-14 01:16:19 +0800

    Add support for videos

--- js/instagram-main.js
+++ js/instagram-main.js
@@ -2,11 +2,19 @@
 
 (() => {
     document.addEventListener('contextmenu', function(e) {
-        let image = e.srcElement.parentElement.querySelectorAll('img');
-        if (image.length === 1) {
+        let media = e.srcElement.parentElement.querySelectorAll('img, video');
+        if (media.length === 1 || (media.length === 2 && (media[0].tagName === 'VIDEO' || media[1].tagName === 'VIDEO'))) {
             e.preventDefault();
 
-            window.open(image[0].src, '_blank');
+            if (media.length === 1) {
+                window.open(media[0].src, '_blank');
+            } else {
+                [].slice.call(media, 0).map(function (m) {
+                    if (m.tagName === 'VIDEO') {
+                        window.open(m.src, '_blank');
+                    }
+                });
+            }
         }
     });
 })();
